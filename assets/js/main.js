fea95a0e53c15094c8e1cbad4ff62e385c7b2d8a
DotunLonge <funnica4@gmail.com>
2017-03-27 11:37:34 +0100

    Sass adjustment commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,21 @@
-$(function(){
 
-    $(".owl-carousel").owlCarousel();
+$(function(){
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+        items:1,
+        loop:true,
+        dots:true,
+        animateIn: 'flipInY',
+        animateOut: 'flipOutY',
+        autoplay:false,
+        autoplayTimeout:1000
+    });
+
+    $(".owl-partners").owlCarousel({
+        items:1,
+        dots:true,
+        margin:10,
+        nav:false
+    });
 
 });
